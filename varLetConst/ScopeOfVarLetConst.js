a433afb071c9c56adfd7efcb0eee0a145d431d79
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-25 09:45:15 +0530

    ecma learning

---
+++ varLetConst/ScopeOfVarLetConst.js
@@ -0,0 +1,21 @@
+
+   
+     let  num= 1;
+    function printHello() {
+        var num= 8;
+        // var num = 7;
+        // let num= 9;
+        console.log(num);  
+       var num= 6;
+       num =3;
+       console.log(num);  
+
+    }
+
+    num =2;
+    console.log(num);
+
+printHello();
+
+console.log(num);
+
---
+++ varLetConst/Var_Let_Const.js
@@ -0,0 +1,17 @@
+const var_let_const= (x) => { console.log(x*x)
+
+    return x+x;
+};
+
+// var_let_const(8);
+console.log(var_let_const(7))
+
+////////////////////////////////////////
+
+function   hiHello(greetings) {
+
+    return greetings + "everyone";
+    
+}
+
+console.log(hiHello("good morning " ));
