3821f9c25772a1188079894e529a42fc9281e29a
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-25 12:39:23 +0530

    swaping value

--- dsaAndAlgorithm/SwapWithout.js
+++ dsaAndAlgorithm/SwapWithout.js
@@ -1,21 +1,16 @@
 const Swap = (n1, n2) => {
+  console.log(`before swaping n1 is ${n1} and n2 is ${n2}`);
 
-    console.log(`before swaping n1 is ${n1} and n2 is ${n2}`)
+  n1 = n1 + n2;
+  console.log(n1);
 
-    n1 = n1 + n2;
-    console.log(n1)
+  n2 = n1 - n2;
+  n1 = n1 - n2;
 
-    n2 = n1 - n2;
-    n1 = n1 - n2;
+  console.log(`now n2 is ${n2} and n1 is ${n1}`);
+};
 
-    console.log(`now n2 is ${n2} and n1 is ${n1}`)
-
-
-
-}
-
-
-n1 = 10;
-n2 = 5;
+let n1 = 10;
+let n2 = 5;
 
 Swap(n1, n2);
