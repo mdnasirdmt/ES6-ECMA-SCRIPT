4eaac3fbe4d2d381dd90981ffc402d8a730f1b06
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-10-07 10:25:24 +0530

    first class function

--- global execution context/functions/firstClassFunction.js
+++ global execution context/functions/firstClassFunction.js
@@ -1,6 +1,20 @@
 function firstClassFunction(params) {
     console.log(`the abiity of of a function that takes argument as function and can return a function 
     then we can say function  is First Class Function`);
+
+    return xyz;
+
+
 }
 
-firstClassFunction();
+function xyz(){
+    console.log('this is statement function');
+}
+
+var x= firstClassFunction(xyz);
+console.log(x);
+x();
+
+
+// the abiity of of a function that takes argument as function and can return a function 
+//     then we can say function  is First Class Function
