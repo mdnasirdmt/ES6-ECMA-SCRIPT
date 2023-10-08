b0eb54990ea97b8fe90a085e8f0be5dbf80051f4
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-10-08 14:18:13 +0530

    express error handling, ejs , routes, codepen,

--- backendRoutes/script.js
+++ backendRoutes/script.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
@@ -6,21 +7,50 @@ app.use(express.static('./public'));
 
 
 app.get('/', function (req, res) {
-  res.render('index')
+    res.render('index')
 })
 
 
 
 app.get('/home', function (req, res) {
     res.render('home')
-  })
+})
 
-  app.get('/login', function (req, res) {
+app.get('/login', function (req, res) {
     res.render('login')
-  })
+})
 
-  app.get('/about', function (req, res) {
-    res.render('about', {srk:'Mohd Shahrukh', anuj: 'Anuj Kumar'})
-  })
+app.get('/about', function (req, res) {
+    res.render('about', { srk: 'Mohd Shahrukh', anuj: 'Anuj Kumar' })
+})
+
+//   app.get('/error1', function (req, res, next) {
+
+//     throw Error('Internal server error try after some time ')
+
+//   })
+
+app.get('/error', function (req, res, next) {
+
+    throw Error('Internal server error, try after some time. ')
+
+})
+
+app.use(function errorHandler1(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+})
+
+//   app.use(function errorHandler (err, req, res, next) {
+//     if (res.headersSent) {
+//       return next(err)
+//     }
+//     res.status(500)
+//     res.render('error1', { error: err })
+//   })
+
 
 app.listen(3001)
